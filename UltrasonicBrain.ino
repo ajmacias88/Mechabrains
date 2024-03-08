68a9eb41ae378b9de799d43ed94f59f686b16626
ireneyeu <146492303+ireneyeu@users.noreply.github.com>
2024-03-07 22:34:31 -0800

    Add files via upload
    hail mary

--- UltrasonicBrain.ino
+++ UltrasonicBrain.ino
@@ -1,21 +1,12 @@
-#define USE_TIMER_2 true
-#include <TimerInterrupt.h>
 
-const int leftUSTrig = 4;
-const int leftUSEcho = 5;
-const int leftOUT = 12;
+#include <Servo.h>
 
-const int rightUSTrig = 8;
-const int rightUSEcho = 9;
-const int rightOUT = 11;
 
-const int frontUSTrig = 2;
-const int frontUSEcho = 3;
-const int frontOUT = 10;
 
-const int backUSTrig = 6;
-const int backUSEcho = 7;
-const int backOUT = 13;
+#define ARM_IN 4
+#define SLIDE_IN  5
+#define ARM_OUT 10
+#define SLIDE_OUT 11
 
 int count = 0;
 int type = 1;
@@ -25,91 +16,35 @@ int state;
 
 float durationF, distanceF, durationR, distanceR, durationL, distanceL, durationB, distanceB;
 
-void TimerHandler1() {
-  if (count == 0) {
-    state = HIGH;
-    digitalWrite(frontUSTrig, state);
-    digitalWrite(rightUSTrig, state);
-    digitalWrite(leftUSTrig, state);
-    digitalWrite(backUSTrig, state);
-  } else {
-    state = LOW;
-    digitalWrite(frontUSTrig, state);
-    digitalWrite(rightUSTrig, state);
-    digitalWrite(leftUSTrig, state);
-    digitalWrite(backUSTrig, state);
-  }
-  count++;
-  if (count == 2) {
-    if (type == 1) {
-      durationF = pulseIn(frontUSEcho, HIGH);
-      distanceF = (durationF * .0343) / 2;
-      Serial.println(distanceF);
-      Serial.println("F");
-      if (distanceF < wallDistance) {
-        digitalWrite(frontOUT, HIGH);
-        digitalWrite(frontOUT, LOW);
-      }
-    } else if (type == 2) {
-      durationL = pulseIn(leftUSEcho, HIGH);
-      distanceL = (durationL * .0343) / 2;
-      Serial.println(distanceL);
-      Serial.println("L");
-      if (distanceL < wallDistance){
-        digitalWrite(leftOUT, HIGH);
-        digitalWrite(leftOUT, LOW);
-      }
-    } else if (type == 3) {
-      durationR = pulseIn(rightUSEcho, HIGH);
-      distanceR = (durationR * .0343) / 2;
-      Serial.println(distanceR);
-      Serial.println("R");
-      if (distanceR < wallDistance){
-        digitalWrite(rightOUT, HIGH);
-        digitalWrite(rightOUT, LOW);
-      }
-    }
-     else if (type == 4) {
-      durationB = pulseIn(backUSEcho, HIGH);
-      distanceB = (durationB * .0343) / 2;
-      Serial.println(distanceB);
-      Serial.println("B");
-      if (distanceB < wallDistance){
-        digitalWrite(backOUT, HIGH);
-        digitalWrite(backOUT, LOW);
-      }
-      type = 0;
-    }
-    type++;
+Servo arm;
 
-    count = 0;
-    }
-
-}
+Servo slide;
 
 void setup() {
-  pinMode(leftUSTrig, OUTPUT);
-  pinMode(leftUSEcho, INPUT);
 
-  pinMode(rightUSTrig, OUTPUT);
-  pinMode(rightUSEcho, INPUT);
+  pinMode(ARM_IN, INPUT);
+  pinMode(SLIDE_IN, INPUT);
 
-  pinMode(frontUSTrig, OUTPUT);
-  pinMode(frontUSEcho, INPUT);
-
-  pinMode(backUSTrig, OUTPUT);
-  pinMode(backUSEcho, INPUT);
-
-  pinMode(leftOUT, OUTPUT);
-  pinMode(rightOUT, OUTPUT);
-  pinMode(frontOUT, OUTPUT);
-  pinMode(backOUT, OUTPUT);
-
-  ITimer2.init();
-  ITimer2.setFrequency(freq1, TimerHandler1);
+  arm.attach(10);
+  slide.attach(11);
 
   Serial.begin(9600);
+  digitalWrite(ARM_IN, HIGH);
 }
 
 void loop() {
-}
+
+
+  if (digitalRead(ARM_IN) == HIGH) {
+    arm.write(110);
+  }
+  if (digitalRead(ARM_IN) == 0) {
+    arm.write(0);
+  }
+  if (digitalRead(SLIDE_IN) == HIGH) {
+    slide.write(110);
+  }
+  if (digitalRead(SLIDE_IN) == LOW) {
+    slide.write(0);
+  }
+}\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
